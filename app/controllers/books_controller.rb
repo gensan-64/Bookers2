e8438575e8da78369560dbff9b57d27bb46a0450
gensan-64 <genyuu6240@gmail.com>
2021-08-12 10:14:12 +0000

    12日夜

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,32 @@
 class BooksController < ApplicationController
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     redirect_to books_path
   end
 
   def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+
   end
 
   def show
+    @book = book_path
+  end
+
+  def edit
+   @book = Book.new
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
   end
+
+  def index
+   @book = Book.all
+  end
+
+  def edit
+  end
+
+  def update
+   
+  end
 end
