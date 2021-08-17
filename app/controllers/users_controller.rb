8e8b0fa860c2dc1de398d7d6a4eae988b6f92072
gensan-64 <genyuu6240@gmail.com>
2021-08-17 06:13:38 +0000

    17日昼

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,34 @@
 class UsersController < ApplicationController
-  def show
-   @user = User.find(params[:id])
-   @book = Book.new
-   @books = @user.books
-  end
+   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+   end
+ 
+   def index
+    @book = Book.all
+    @book = Book.new
+    @user = current_user
+    @users = User.all
+   end
+   
+   def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+   end
 
-  def index
-   @book = Book.all
-   @book = Book.new
-   @user = current_user
-   @users = User.all
-  end
-
-  def edit
-  end
-
-  def update
-
-  end
+   def edit
+    @user = User.find(params[:id])
+   end
+   
+   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+   end
+   
+   def user_params
+     params.require(:user).permit(:name, :profile_image)
+   end
 end
